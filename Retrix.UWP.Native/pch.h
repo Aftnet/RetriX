7458eb56b3f93bf39fa6cf1d75370a6844c0a34e
Alberto Fustinoni <alberto@aftnet.net>
2018-06-26 23:21:02 +0900

    Cleanup

--- Retrix.UWP.Native/pch.h
+++ Retrix.UWP.Native/pch.h
@@ -1,15 +1,9 @@
 ﻿#pragma once
 
-#include <collection.h>
-#include <ppltasks.h>
-
 // DirectX headers.
 #include <d3d11.h>
+#include <d3dcompiler.h>
 #include <d2d1_3.h>
-#include <d2d1_1.h>
-#include <d2d1_2.h>
-#include <d2d1helper.h>
-#include <DirectXMath.h>
 #include <Windows.Graphics.DirectX.Direct3D11.interop.h>
 
 // Win2D headers.

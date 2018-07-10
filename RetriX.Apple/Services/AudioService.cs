2f4babe92d4d3a487a3366b0371b25536e95d773
Alberto Fustinoni <claudiuslollarius@gmail.com>
2018-07-10 17:23:03 +0900

    Logic

--- RetriX.Apple/Services/AudioService.cs
+++ RetriX.Apple/Services/AudioService.cs
@@ -1,30 +1,48 @@
 ﻿using AudioToolbox;
-using LibRetriX;
 using RetriX.Shared.Services;
-using System;
 using System.Threading.Tasks;
 
 namespace RetriX.Apple.Services
 {
     public sealed class AudioService : AudioServiceBase
     {
-        public void TimingChanged(SystemTimings timings)
+        private OutputAudioQueue queue;
+        private OutputAudioQueue Queue
+        {
+            get => queue;
+            set { if (queue != value) { queue?.Dispose(); queue = value; } }
+        }
+
+        protected override Task CreateResourcesAsync(uint sampleRate)
         {
             var description = new AudioStreamBasicDescription(AudioFormatType.LinearPCM)
             {
                 BitsPerChannel = 8 * sizeof(short),
                 FormatFlags = AudioFormatFlags.IsSignedInteger | AudioFormatFlags.IsPacked,
-                ChannelsPerFrame = NumChannels,
+                ChannelsPerFrame = (int)NumChannels,
                 FramesPerPacket = 1,
-                SampleRate = timings.SampleRate,
-                BytesPerFrame = NumChannels * sizeof(short)
+                SampleRate = sampleRate,
+                BytesPerFrame = (int)NumChannels * (int)SampleSizeBytes
             };
 
-            lock (QueueLock)
-            {
-                Queue?.Stop(true);
-                Queue = new OutputAudioQueue(description);
-            }
+            Queue = new OutputAudioQueue(description);
+            return Task.CompletedTask;
+        }
+
+        protected override void DestroyResources()
+        {
+            Queue = null;
+        }
+
+        protected override void StartPlayback()
+        {
+            Queue.Start();
+        }
+
+        protected override void StopPlayback()
+        {
+            Queue.Stop(true);
+            Queue.Reset();
         }
     }
 }
